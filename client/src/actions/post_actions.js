fa5e8f888a6b0e811db8024d114688518feb6f63
Julio Mojica <juliocmojica@gmail.com>
2018-02-03 16:42:51 -0500

    voting on post

--- client/src/actions/post_actions.js
+++ client/src/actions/post_actions.js
@@ -1,5 +1,5 @@
 export const GET_ALL_POSTS = 'GET_ALL_POSTS';
-
+export const FETCH_A_POST = 'FETCH_A_POST';
 
 
 const headers =   {headers: { 'Authorization': 'whatever-you-want' }}
@@ -20,3 +20,36 @@ export function fetch_all_posts(dispatch) {
 	}
 }
 
+
+export function fetchAPost(post) {
+	return {
+		type: FETCH_A_POST,
+		post
+	}
+}
+
+export function fetchin_a_post(id, dispatch) {
+	return dispatch => {
+		fetch(`http://localhost:3001/posts/${id}`, headers)
+		.then(response => response.json())
+		.then(post => dispatch(fetchAPost(post)))
+	}
+}
+
+export function voting_on_post(id, option, dispatch) {
+	return dispatch => {
+		let vote = `${option}`;
+		fetch(`http://localhost:3001/posts/${id}`, {
+			headers: {
+               'Authorization': 'whatever-you-want',
+               'Accept': 'application/json',
+               "Content-Type": "application/json",
+           },
+           method: 'Post',
+           body: JSON.stringify({option: vote})
+		})
+		.then(response => response.json())
+		.then(json => dispatch(fetch))
+
+	}
+}
